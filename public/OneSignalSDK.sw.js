9551e90919a2aceaa9fc4381db724c9147f525eb
Peter Eshak Abdo <mixcolor11@gmail.com>
2025-10-09 01:21:25 +0300

    update version of onsignal importScripts

--- public/OneSignalSDK.sw.js
+++ public/OneSignalSDK.sw.js
@@ -1,2 +1,1 @@
-// OneSignal Service Worker (v15)
-importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
 // OneSignal Service Worker
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
